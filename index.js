15ecf67d78b98b672314266096b10189e184ee8a
Anandadevnath <anandadevnath473@gmail.com>
2025-12-30 22:42:12 +0600

    Update API version in welcome message to 1.0.0

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.json({
         message: " Welcome to Shirayuki Anime Scraper!",
-        version: "2.0.0",
+        version: "1.0.0",
         endpoints: [
             {
                 home: [
